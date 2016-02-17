05ad2cebf575eeec7be1b263ae341594182f6a7d
George Wooding <georgewooding@hotmail.co.uk>
2016-02-17 19:20:00 +0000

    Create PalindromeChecker.cpp

---
+++ PalindromeChecker.cpp
@@ -0,0 +1,52 @@
+#include "stdafx.h"
+#include <iostream>
+#include <string>
+
+using std::string;
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main()
+{	
+	string word;
+	cin >> word;
+
+	for (string::size_type i = 0; i < word.size()/2; i++) {
+		if (word[i] != word[word.size() - 1 - i]){
+			cout <<"Not palindrome"<<endl;
+			break;
+		}
+	}
+	system("pause"); 
+	cout << "Goodbye" << endl;
+
+    return 0;
+}
+
+#include "stdafx.h"
+#include <iostream>
+#include <string>
+
+using std::string;
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main()
+{	
+	string word;
+	cin >> word;
+
+	for (string::size_type i = 0; i < word.size()/2; i++) {
+		if (word[i] != word[word.size() - 1 - i]){
+			cout <<"Not palindrome"<<endl;
+			break;
+		}
+	}
+	system("pause"); 
+	cout << "Goodbye" << endl;
+
+    return 0;
+}
+
