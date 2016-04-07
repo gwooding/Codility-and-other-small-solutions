f324758d079371f9827e872ae68a395117bfd06d
George Wooding <georgewooding@hotmail.co.uk>
2016-04-07 07:36:16 +0100

    Added files via upload

---
+++ BlockingCollectionProducerConsumer/BlockingCollectionProducerConsumer/Program.cs
@@ -0,0 +1,27 @@
+﻿
+using System;
+using System.Threading.Tasks;
+
+namespace BlockingCollectionProducerConsumer
+{
+    public class ParallelTaskRunner
+    {
+        public static void Main()
+        {
+            var consumer = ParallelTaskConsumer.StartNew(10, Console.WriteLine);
+
+            Task.Factory.StartNew(() =>
+            {
+                for (int i = 0; i < 14; i++)
+                {
+                    int j = i;
+                    consumer.EnqueueTask(new Task(() => Console.WriteLine(j)));
+                }
+
+                consumer.Dispose();
+            });
+
+            Console.ReadLine();
+        }
+    }
+}
